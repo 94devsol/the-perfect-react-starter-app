27e060c7d300a2a346a43e66fae29bb7ce2be2c6
RuTa6 <mailruhma1997@gmail.com>
2022-01-04 15:14:29 +0530

    added toast provider

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import ThemedSuspense from "./components/ThemedSuspense";
 import { Windmill } from "@windmill/react-ui";
 import * as serviceWorker from "./serviceWorker";
 import AuthProvider from "./context/AuthContext";
+import { ToastProvider } from "react-toast-notifications";
+
 // if (process.env.NODE_ENV !== 'production') {
 //   const axe = require('react-axe')
 //   axe(React, ReactDOM, 1000)
@@ -14,13 +16,15 @@ import AuthProvider from "./context/AuthContext";
 
 ReactDOM.render(
   <AuthProvider>
-    <SidebarProvider>
-      <Suspense fallback={<ThemedSuspense />}>
-        <Windmill usePreferences>
-          <App />
-        </Windmill>
-      </Suspense>
-    </SidebarProvider>
+    <ToastProvider>
+      <SidebarProvider>
+        <Suspense fallback={<ThemedSuspense />}>
+          <Windmill usePreferences>
+            <App />
+          </Windmill>
+        </Suspense>
+      </SidebarProvider>
+    </ToastProvider>
   </AuthProvider>,
   document.getElementById("root")
 );
